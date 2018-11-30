7dc04a2e56ed929819ffd58dbd7a5886d6e18a4d
HeavenVolkoff <HeavenVolkoff@users.noreply.github.com>
2018-11-30 17:51:07 -0200

    Version 1.0.0b6 (Beta 6)  - Added "attempt_await"

---
+++ src/async_tools/attempt_await.py
@@ -0,0 +1,19 @@
+__all__ = ("attempt_await",)
+
+# Internal
+import typing as T
+
+# Generic Types
+from asyncio import AbstractEventLoop, ensure_future
+
+K = T.TypeVar("K")
+L = T.TypeVar("L")
+
+
+async def attempt_await(awaitable: T.Union[K, T.Awaitable[K]], loop: AbstractEventLoop) -> K:
+    try:
+        result_fut = ensure_future(T.cast(T.Awaitable[K], awaitable), loop=loop)
+    except TypeError:
+        return T.cast(K, awaitable)  # Not an awaitable
+    else:
+        return await result_fut
