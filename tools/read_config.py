aa6f52d490cc959499cc126b8cc0aaf1d24abe48
HeavenVolkoff <HeavenVolkoff@users.noreply.github.com>
2019-01-31 19:00:12 -0200

    Updated foundation

--- tools/read_config.py
+++ tools/read_config.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
 
 # Internal
-import sys
-from itertools import chain
 from configparser import ConfigParser
-
-
-def read_config(file, ctx, prop):
-    parser = ConfigParser()
-    with open(file, encoding="utf8") as config_file:
-        fixed_config_file = chain(("[__TOP__]",), config_file)
-        parser.read_file(fixed_config_file)
-
-    return parser.get(ctx, prop, fallback="")
-
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         print("Missing arguments", file=sys.stderr)
         sys.exit(-1)
 
-    print(read_config(sys.argv[1], sys.argv[2], sys.argv[3]))
+    from itertools import chain
+
+    parser = ConfigParser()
+    with open(sys.argv[1], encoding="utf8") as config_file:
+        fixed_config_file = chain(("[__TOP__]",), config_file)
+        parser.read_file(fixed_config_file)
+
+    print(parser.get(sys.argv[2], sys.argv[3], fallback=""))
