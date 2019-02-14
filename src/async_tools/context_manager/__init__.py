b3d4671a3e4655c17c6d50bd98d3fa6e595ef53b
HeavenVolkoff <HeavenVolkoff@users.noreply.github.com>
2019-02-14 15:53:18 -0200

    Version 1.1.0b1 (Beta 1) - Added async_context_manager compatible with Python >= 3.5

--- src/async_tools/context_manager/__init__.py
+++ src/async_tools/context_manager/__init__.py
@@ -2,6 +2,7 @@
 from async_tools.abstract import AsyncContextManager
 
 # Project
+from .async_context_manager import asynccontextmanager
 from .async_generator_close_context import AsyncGeneratorCloseContext as aclosing
 
-__all__ = ("aclosing", "AsyncContextManager")
+__all__ = ("aclosing", "AsyncContextManager", "asynccontextmanager")
