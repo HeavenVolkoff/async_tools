a8a3a3a6ae56ae20ec885952d07ce4767e5c0cea
HeavenVolkoff <HeavenVolkoff@users.noreply.github.com>
2019-07-20 03:21:37 -0300

    Fix incorrect changes in all_tasks from commit 8b2e288

--- src/async_tools/all_tasks.py
+++ src/async_tools/all_tasks.py
@@ -2,8 +2,8 @@
 import asyncio
 
 if hasattr(asyncio, "all_tasks"):
-    current_task = asyncio.all_tasks
+    all_tasks = asyncio.all_tasks
 else:
-    current_task = asyncio.Task.all_tasks
+    all_tasks = asyncio.Task.all_tasks
 
 __all__ = ("all_tasks",)
