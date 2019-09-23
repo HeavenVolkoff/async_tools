9a0fc6a14ecb0b4b16f01637f447b615ede7da5c
HeavenVolkoff <vasconcellos.dev@gmail.com>
2019-09-23 20:21:15 -0300

    Improve tests for wait_with_care

--- tests/test_wait_with_care.py
+++ tests/test_wait_with_care.py
@@ -1,6 +1,7 @@
 # Internal
 import asyncio
 import unittest
+from asyncio import CancelledError
 
 # External
 import asynctest
@@ -8,6 +9,14 @@ from async_tools import wait_with_care
 
 
 class WaitWithCareTestCase(asynctest.TestCase, unittest.TestCase):
+    async def test_wait_with_care_empty(self):
+        done, pending = await wait_with_care()
+
+        self.assertEqual(len(done), 0)
+        self.assertEqual(len(pending), 0)
+        self.assertIsInstance(done, set)
+        self.assertIsInstance(pending, set)
+
     async def test_wait_with_care_simple(self):
         async def long_running_task():
             await asyncio.sleep(0.01)
@@ -17,6 +26,8 @@ class WaitWithCareTestCase(asynctest.TestCase, unittest.TestCase):
 
         self.assertFalse(pending)
         self.assertEqual(len(done), 1)
+        self.assertIsInstance(done, set)
+        self.assertIsInstance(pending, set)
 
         task, = done
 
@@ -31,6 +42,35 @@ class WaitWithCareTestCase(asynctest.TestCase, unittest.TestCase):
 
         self.assertFalse(pending)
         self.assertEqual(len(done), 100)
+        self.assertIsInstance(done, set)
+        self.assertIsInstance(pending, set)
 
         for i, fut in enumerate(done):
             self.assertEqual(fut.result(), futures.index(fut))
+
+    async def test_wait_with_care_cancelled(self):
+        futures = [self.loop.create_future() for _ in range(100)]
+        cancelled_future = self.loop.create_future()
+
+        cancelled_future.cancel()
+
+        for i, fut in enumerate(futures):
+            fut.set_result(i)
+
+        with self.assertRaises(CancelledError):
+            _, __ = await wait_with_care(cancelled_future)
+
+        with self.assertRaises(CancelledError):
+            _, __ = await wait_with_care(*futures, cancelled_future)
+
+        done, pending = await wait_with_care(cancelled_future, ignore_cancelled=True)
+
+        self.assertFalse(pending)
+        self.assertEqual(len(done), 1)
+        self.assertIsInstance(done, set)
+        self.assertIsInstance(pending, set)
+
+        cancelled_task, = done
+
+        with self.assertRaises(CancelledError):
+            cancelled_task.result()
